033f087bebb9ec8be850c33ff5ea29c13b3c218c
Saparali <saparali02001@gmail.com>
2021-10-01 13:56:48 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//alert('Hello');
+
+// const result =confirm("Are you now Saparali?");
+// console.log (result);
+
+// const answer = +prompt(" Are you now mi?");
+// console.log (answer +10);
+
+// const answers = [];
+// answers[0] = prompt(" Как ваше имя?");
+// answers[1] = prompt(" Как ваше фамилия?");
+// answers[2] = prompt(" Ваш телефон номер?");
+
+// document.write (answers);
+// const category = 'toys';
+// console.log(`http://someurl.com/${category}/5`);
+// const user = "Saparali"
+// alert(`привет,${user}`);
+
+// let incr = 10,
+//     decr = 10;
+// incr++;
+// decr--;
+// console.log(incr);
+// console.log(decr);
+
+const isChecked = false,
+      isClose = true;
+console.log(!isChecked || !isClose) ;     
+
+
+
+
+
+
+
+
+
+
+
+
+
